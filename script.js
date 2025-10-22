46a5516830b5ae46206a44fd559258a961a904aa
AbduHacker <199925619+AbduHacker@users.noreply.github.com>
2025-10-22 11:51:33 +0530

    Add functionalities and more styles

---
+++ script.js
@@ -0,0 +1,31 @@
+let container = document.querySelector(".container");
+function getRandomHexColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  // Pad with leading zeros if the hex code is less than 6 digits
+  return `#${randomColor.padStart(6, '0')}`;
+}
+
+function createSquares(n) {
+  const CONTAINER_SIZE = container.clientHeight;
+  let squareSize = CONTAINER_SIZE / n;
+  for (let i = 1; i <= (n ** 2); i++) {
+    let square = document.createElement("div");
+    square.style.cssText = `width: ${squareSize - 1}px; height: ${squareSize - 1};`
+    square.classList.add("square");
+    square.id = `square-${i}`;
+    container.appendChild(square);
+  }
+  
+}
+createSquares(2);
+function changeColor() {
+  container.addEventListener("mouseover", (event) => {
+    let target = event.target;
+    if (target.className === "square") {
+      let random = getRandomHexColor();
+      target.style.backgroundColor = random;
+    }
+  });
+}
+changeColor();
+
